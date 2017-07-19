2543ac654c794f16e7a150e60c98251dc70cef8c
AntSworD <zhengjj.asd@gmail.com>
2017-07-19 18:23:05 +0800

    test

--- services/hello.js
+++ services/hello.js
@@ -4,5 +4,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 exports.handler = (event, ctx, callback) => {
   console.log('invoke fc_helloworld!');
-  callback(null, `Hello world! NODE_ENV: ${process.env.NODE_ENV}`);
+  callback(null, {
+    body: `Hello world! NODE_ENV: ${process.env.NODE_ENV}`
+  });
 };
